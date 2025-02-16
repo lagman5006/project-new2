429c432a16fef224cb8356d08127b3aa08b23cc8
lagman <asliddin5006>
2025-02-16 23:54:34 +0500

    o'zgarish

--- py.py
+++ py.py
@@ -2,6 +2,6 @@ user1 = int(input("son kriting xoxlagan:"))
 if user1 < 0:
     print("son 0 dan kchik ekan")
 elif user1 %2 ==0:
-    print("son jup ekan")
+    print("son juft ekan")
 elif not user1 % 2 == 0:
-    pass
+    print("son juft emas")
